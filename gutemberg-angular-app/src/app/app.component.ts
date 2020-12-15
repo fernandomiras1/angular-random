8d78fe2c9c005af149bb44b1d037a2319bd2b4d8
fernandomiras1 <fernando.miras.pc@gmail.com>
2020-12-15 12:21:57 -0300

    feat: css app main

--- gutemberg-angular-app/src/app/app.component.ts
+++ gutemberg-angular-app/src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'gutemberg-angular-app';
+  
+  selectedClass = 'phone-320';
+  selectedView = { domain: 'cards', viewId: 'cards-list', title: 'list of cards', selected: true};
+
+
+  screenSizes = [
+    { class: 'phone-320', text: 'small 320 x 568', selected: true},
+    { class: 'phone-360', text: 'medium 360 x 640', selected: false},
+    { class: 'phone-414', text: 'big 414 x 896', selected: false},
+  ];
+
+  constructor() {}
+
+  selectSize(item: any): void {
+    this.screenSizes.forEach(size => size.selected = (item.class === size.class));
+    this.selectedClass = item.class;
+  }
+
 }
