d4647dea38d99aca343580129f67249a2f64486c
Fernando Miras <fmiras@simtlix.com>
2024-03-27 09:52:31 -0300

    'angular-input-router-binding-main'

--- cursos-decoded-frontend/angular-input-router-binding-main/src/app/app.config.ts
+++ cursos-decoded-frontend/angular-input-router-binding-main/src/app/app.config.ts
@@ -3,6 +3,16 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 
+
+
+/**
+ * 
+ * withComponentInputBinding
+ * 
+ * Tenemos que acticar esto para que funcione el binding por inputs
+ * 
+ * 
+ */
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(
