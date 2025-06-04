d94a10a05d96dfbc483e3d8f0f2f6d9c8e365588
Fernando Miras <fmiras@wellright.com>
2025-06-04 08:40:09 -0500

    angular-linked-signal-19.0

--- cursos-decoded-frontend/angular-linked-signal-19.0/src/app/rating-picker/reaction-picker.ts
+++ cursos-decoded-frontend/angular-linked-signal-19.0/src/app/rating-picker/reaction-picker.ts
@@ -16,12 +16,27 @@ import { Component, computed, input, linkedSignal, signal } from '@angular/core'
 export class ReactionPicker {
   reactions = input<string[]>([]);
 
+  // El primer parametro es el valor inicial, el segundo es el tipo de dato por eso es un array de strings
+  // el es valor que va a regrese el método selectedReaction()
   selectedReaction = linkedSignal<string[], string | null>({
     source: () => this.reactions(),
     computation: (source, previous) => {
+      console.log('Previous:', previous);
       return source.find(r => previous?.value === r) || null;
     }
   });
+
+
+  /**
+   * 1 ) Es como el Computed pero nos permite hacer un override
+   * del valor inicial.
+   * En este caso, queremos que el valor inicial sea el último
+   * de la lista de reacciones, o null si no hay reacciones. Pero podemos hacer un set y cambiarlo
+   * a otro valor.
+   *
+   * 2) linkedSignal nos provee ecceso al valor previous value
+   */
+  // selectedReaction = linkedSignal(() => this.reactions().at(-1) || null);
 
   protected isSelected(reaction: string){
     return this.selectedReaction() === reaction;
